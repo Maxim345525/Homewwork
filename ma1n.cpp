609146a4d50d90f3474ef7be84bc5930dfcce662
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-11 19:59:56 +0200

    add code

---
+++ ma1n.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	double R, S;
+	cout << "¬вед≥ть рад≥ус кола: ";
+	cin >> R;
+	S = 3.14 * R * R;
+	cout << "ѕлоща кола = " << S << "" << endl;
+	return 0;
+}
