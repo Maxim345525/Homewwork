9c7f6d3f25d059d924230ace3b23b9b20b788ad4
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-12 22:05:36 +0200

    add code

---
+++ ma1n.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	double v, S, a, t;
+	cout << "Введіть швидкість(v): ";
+	cout << "Введіть час(t): ";
+	cout << "Введіть прискорення(a): ";
+	cin >> S;
+	S = 2 * 13 + (32 * 13 * 13) / 2;
+	cout << "Пройдена відстань = " << S << "" << endl;
+	return 0;
+}
